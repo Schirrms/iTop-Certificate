febd88a6359de7af1a0f754c10ee9c399d7e1afd
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-10-27 17:25:17 +0200

    :sparkles: attempt to revert to Get function

--- schirrms-certificate/main.schirrms-certificate.class.inc.php
+++ schirrms-certificate/main.schirrms-certificate.class.inc.php
@@ -9,7 +9,38 @@ class AttributeRemainingDays extends AttributeString
      * @inheritdoc
      */
 
-    public function GetValueLabel($sValue)
+    public function Get($sValue, $oHostObject = null)
+    {
+        $sRemaining = parent::Get($sValue);
+        if ( !is_null($oHostObject) ) {
+            $sNewRemaining = '';
+            $dExpDate = $oHostObject->Get('expiration_date');
+            if ( !is_null( $dExpDate ) ) {
+                $dExpiration = new DateTime($dExpDate);
+                $dControl = new DateTime('');
+                $datectrl = $dControl->diff($dExpiration);
+                if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
+                    $dDateNow = new DateTime(date('Y-m-d'));
+                    $interval = $dDateNow->diff($dExpiration);
+                    $sNewRemaining = $interval->format('%r%a');
+                }
+            }
+            if ( strcmp($sNewRemaining, $sRemaining) !== 0 ) {
+                $oHostObject->Set('expiration_days', $sNewRemaining);
+                $oHostObject->DBWrite();
+            }
+            return $sNewRemaining;
+        }
+        else {
+            return $sRemaining;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+
+    /* public function GetValueLabel($sValue)
     {
         $sRemaining = parent::GetValueLabel($sValue);
         $sNewRemaining = '';
@@ -28,18 +59,18 @@ class AttributeRemainingDays extends AttributeString
         }
     return $sNewRemaining;
     }
+    */
 
     /**
      * @inheritdoc
      */
-    public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
+    /* public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
     {
         $sRemaining = parent::GetAsHTML($sValue, $oHostObject, $bLocalize);
         if ( !is_null($oHostObject) ) {
-            $sNewRemaining = 'Test';
+            $sNewRemaining = '';
             $dExpDate = $oHostObject->Get('expiration_date');
             if ( !is_null( $dExpDate ) ) {
-                $sNewRemaining = "Test - $dExpDate";
                 $dExpiration = new DateTime($dExpDate);
                 $dControl = new DateTime('');
                 $datectrl = $dControl->diff($dExpiration);
@@ -49,18 +80,14 @@ class AttributeRemainingDays extends AttributeString
                     $sNewRemaining = $interval->format('%r%a');
                 }
             }
-            else {
-                $sNewRemaining = 'Test - null - '.$oHostObject->Get('expiration_date').' - '.$oHostObject->GetKey();
-            }
             if ( strcmp($sNewRemaining, $sRemaining) !== 0 ) {
                 $oHostObject->Set('expiration_days', $sNewRemaining);
                 $oHostObject->DBWrite();
-                $sNewRemaining .= " +Write";
             }
             return $sNewRemaining;
         }
         else {
             return $sRemaining;
         }
-    }
+    } */
 }
