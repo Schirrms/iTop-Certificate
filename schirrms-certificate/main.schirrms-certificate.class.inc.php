85c307550a1a7eb611e4910378b808f065642607
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-10-27 13:11:02 +0200

    :bug: transform Get in GetValueLabel and GetAsHTML

--- schirrms-certificate/main.schirrms-certificate.class.inc.php
+++ schirrms-certificate/main.schirrms-certificate.class.inc.php
@@ -9,25 +9,46 @@ class AttributeRemainingDays extends AttributeString
      * @inheritdoc
      */
 
-    public function Get($sValue)
+    public function GetValueLabel($sValue)
     {
-        $sRemaining = parent::Get($sValue);
+        $sRemaining = parent::GetValueLabel($sValue);
         $sNewRemaining = '';
-        if ( defined ( parent::Get('expiration_date') ) ) {
+        if ( defined ( $this->Get('expiration_date') ) ) {
             $dExpiration = new DateTime($this->Get('expiration_date'));
             $dControl = new DateTime('');
             $datectrl = $dControl->diff($dExpiration);
-
             if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
                 $dDateNow = new DateTime(date('Y-m-d'));
                 $interval = $dDateNow->diff($dExpiration);
                 $sNewRemaining = $interval->format('%r%a');
                 if ( $sNewRemaining != $sRemaining ) {
-                    parent::Set('expiration_days', $sNewRemaining);
+                    $this->Set('expiration_days', $sNewRemaining);
                 }
             }
         }
+        return $sNewRemaining;
+    }
 
+    /**
+     * @inheritdoc
+     */
+    public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
+    {
+        $sRemaining = parent::GetAsHTML($sValue, $oHostObject, $bLocalize);
+        $sNewRemaining = '';
+        if ( defined ( $this->Get('expiration_date') ) ) {
+            $dExpiration = new DateTime($this->Get('expiration_date'));
+            $dControl = new DateTime('');
+            $datectrl = $dControl->diff($dExpiration);
+            if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
+                $dDateNow = new DateTime(date('Y-m-d'));
+                $interval = $dDateNow->diff($dExpiration);
+                $sNewRemaining = $interval->format('%r%a');
+                if ( $sNewRemaining != $sRemaining ) {
+                    $this->Set('expiration_days', $sNewRemaining);
+                }
+            }
+        }
         return $sNewRemaining;
     }
 }
