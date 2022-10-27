d7500203746dd8564c15a82e7a03760b81962f15
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-10-27 16:13:48 +0200

    :bug: try to use oHostObject

--- schirrms-certificate/main.schirrms-certificate.class.inc.php
+++ schirrms-certificate/main.schirrms-certificate.class.inc.php
@@ -35,20 +35,25 @@ class AttributeRemainingDays extends AttributeString
     public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
     {
         $sRemaining = parent::GetAsHTML($sValue, $oHostObject, $bLocalize);
-        $sNewRemaining = '';
-        if ( defined ( parent::Get('expiration_date') ) ) {
-            $dExpiration = new DateTime(parent::Get('expiration_date'));
-            $dControl = new DateTime('');
-            $datectrl = $dControl->diff($dExpiration);
-            if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
-                $dDateNow = new DateTime(date('Y-m-d'));
-                $interval = $dDateNow->diff($dExpiration);
-                $sNewRemaining = $interval->format('%r%a');
+        if ( ! is_null($oHostObject) ) {
+            $sNewRemaining = '';
+            if ( defined ( $oHostObject->Get('expiration_date') ) ) {
+                $dExpiration = new DateTime($oHostObject->Get('expiration_date'));
+                $dControl = new DateTime('');
+                $datectrl = $dControl->diff($dExpiration);
+                if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
+                    $dDateNow = new DateTime(date('Y-m-d'));
+                    $interval = $dDateNow->diff($dExpiration);
+                    $sNewRemaining = $interval->format('%r%a');
+                }
             }
+            if ( $sNewRemaining != $sRemaining ) {
+                $oHostObject->Set('expiration_days', $sNewRemaining);
+            }
+            return $sNewRemaining;
         }
-        if ( $sNewRemaining != $sRemaining ) {
-            parent::Set('expiration_days', $sNewRemaining);
+        else {
+            return $sRemaining;
         }
-        return $sNewRemaining;
     }
 }
