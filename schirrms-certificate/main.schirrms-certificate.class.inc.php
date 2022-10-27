fde44023b08d40b0ae0ce63d53aa36459bbfdebf
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-10-27 17:41:15 +0200

    :sparkles: first attempt ti factorize code

--- schirrms-certificate/main.schirrms-certificate.class.inc.php
+++ schirrms-certificate/main.schirrms-certificate.class.inc.php
@@ -9,85 +9,73 @@ class AttributeRemainingDays extends AttributeString
      * @inheritdoc
      */
 
+    private function UpdateRemainingDays($sRemaining, $oHostObject = null)
+    {
+        if ( !is_null($oHostObject) ) {
+            $sNewRemaining = '';
+            $dExpDate = $oHostObject->Get('expiration_date');
+            if ( !is_null( $dExpDate ) ) {
+                $dExpiration = new DateTime($dExpDate);
+                $dControl = new DateTime('');
+                $datectrl = $dControl->diff($dExpiration);
+                if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
+                    $dDateNow = new DateTime(date('Y-m-d'));
+                    $interval = $dDateNow->diff($dExpiration);
+                    $sNewRemaining = $interval->format('%r%a');
+                }
+            }
+            if ( strcmp($sNewRemaining, $sRemaining) !== 0 ) {
+                $oHostObject->Set('expiration_days', $sNewRemaining);
+                $oHostObject->DBWrite();
+            }
+            return $sNewRemaining;
+        }
+        else {
+            return $sRemaining;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+
     public function Get($sValue, $oHostObject = null)
     {
         $sRemaining = parent::Get($sValue);
-        if ( !is_null($oHostObject) ) {
-            $sNewRemaining = '';
-            $dExpDate = $oHostObject->Get('expiration_date');
-            if ( !is_null( $dExpDate ) ) {
-                $dExpiration = new DateTime($dExpDate);
-                $dControl = new DateTime('');
-                $datectrl = $dControl->diff($dExpiration);
-                if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
-                    $dDateNow = new DateTime(date('Y-m-d'));
-                    $interval = $dDateNow->diff($dExpiration);
-                    $sNewRemaining = $interval->format('%r%a');
-                }
-            }
-            if ( strcmp($sNewRemaining, $sRemaining) !== 0 ) {
-                $oHostObject->Set('expiration_days', $sNewRemaining);
-                $oHostObject->DBWrite();
-            }
-            return $sNewRemaining;
-        }
-        else {
-            return $sRemaining;
-        }
+        
+        return UpdateRemainingDays($sRemaining, $oHostObject);
     }
 
     /**
      * @inheritdoc
      */
 
-    /* public function GetValueLabel($sValue)
+    public function GetValueLabel($sValue, $oHostObject = null)
     {
         $sRemaining = parent::GetValueLabel($sValue);
-        $sNewRemaining = '';
-        if ( defined ( parent::Get('expiration_date') ) ) {
-            $dExpiration = new DateTime(parent::Get('expiration_date'));
-            $dControl = new DateTime('');
-            $datectrl = $dControl->diff($dExpiration);
-            if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
-                $dDateNow = new DateTime(date('Y-m-d'));
-                $interval = $dDateNow->diff($dExpiration);
-                $sNewRemaining = $interval->format('%r%a');
-            }
-        }
-        if ( $sNewRemaining != $sRemaining ) {
-            parent::Set('expiration_days', $sNewRemaining);
-        }
-    return $sNewRemaining;
+
+        return UpdateRemainingDays($sRemaining, $oHostObject);
     }
-    */
 
     /**
      * @inheritdoc
      */
-    /* public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
+    
+    public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
     {
         $sRemaining = parent::GetAsHTML($sValue, $oHostObject, $bLocalize);
-        if ( !is_null($oHostObject) ) {
-            $sNewRemaining = '';
-            $dExpDate = $oHostObject->Get('expiration_date');
-            if ( !is_null( $dExpDate ) ) {
-                $dExpiration = new DateTime($dExpDate);
-                $dControl = new DateTime('');
-                $datectrl = $dControl->diff($dExpiration);
-                if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
-                    $dDateNow = new DateTime(date('Y-m-d'));
-                    $interval = $dDateNow->diff($dExpiration);
-                    $sNewRemaining = $interval->format('%r%a');
-                }
-            }
-            if ( strcmp($sNewRemaining, $sRemaining) !== 0 ) {
-                $oHostObject->Set('expiration_days', $sNewRemaining);
-                $oHostObject->DBWrite();
-            }
-            return $sNewRemaining;
-        }
-        else {
-            return $sRemaining;
-        }
-    } */
+
+        return UpdateRemainingDays($sRemaining, $oHostObject);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    
+    public GetAsXML($sAttCode, $bLocalize = true)
+    {
+        $sRemaining = parent::GetAsXML($sValue, $bLocalize = true);
+
+        return UpdateRemainingDays($sRemaining, $oHostObject);
+    }
 }
