189c6e91a873fcc34996b8ecb12a860826cc5e4b
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-10-27 11:44:47 +0200

    :bug: missing field control

--- schirrms-certificate/main.schirrms-certificate.class.inc.php
+++ schirrms-certificate/main.schirrms-certificate.class.inc.php
@@ -12,17 +12,19 @@ class AttributeRemainingDays extends AttributeString
     public function Get($sValue)
     {
         $sRemaining = parent::Get($sValue);
-        $dExpiration = new DateTime($this->Get('expiration_date'));
-        $dControl = new DateTime('');
-        $datectrl = $dControl->diff($dExpiration);
         $sNewRemaining = '';
+        if ( defined ( $this->Get('expiration_date') ) ) {
+            $dExpiration = new DateTime($this->Get('expiration_date'));
+            $dControl = new DateTime('');
+            $datectrl = $dControl->diff($dExpiration);
 
-        if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
-            $dDateNow = new DateTime(date('Y-m-d'));
-            $interval = $dDateNow->diff($dExpiration);
-            $sNewRemaining = $interval->format('%r%a');
-            if ( $sNewRemaining != $sRemaining ) {
-                $this->Set('expiration_days', $sNewRemaining);
+            if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
+                $dDateNow = new DateTime(date('Y-m-d'));
+                $interval = $dDateNow->diff($dExpiration);
+                $sNewRemaining = $interval->format('%r%a');
+                if ( $sNewRemaining != $sRemaining ) {
+                    $this->Set('expiration_days', $sNewRemaining);
+                }
             }
         }
 
