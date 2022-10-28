aa21547abf449ca8950fd3fefb8143aeba8e370b
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-10-28 18:13:55 +0200

    :bug: revert expiration_days to date

--- schirrms-certificate/main.schirrms-certificate.class.inc.php
+++ schirrms-certificate/main.schirrms-certificate.class.inc.php
@@ -5,59 +5,59 @@
  */
 class AttributeRemainingDays extends AttributeDate
 {
-    /**
-     * @inheritdoc
-     */
+	/**
+	 * @inheritdoc
+	 */
 
-    private function FormatRemainingDays($dRemaining)
-    {
-        $sRemaining = '';
-        if ( $dRemaining !== null && preg_match('/^\d{2,4}-\d{1,2}-\d{1,2}$/', $dRemaining) ) {
-            $dExpiration = new DateTime($dRemaining);
-            $dControl = new DateTime('');
-            $datectrl = $dControl->diff($dExpiration);
-            if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
-                $dDateNow = new DateTime(date('Y-m-d'));
-                $interval = $dDateNow->diff($dExpiration);
-                $sRemaining = $interval->format('%r%a');
-            }
-        }
-        return $sRemaining;
-    }
+	private function FormatRemainingDays($dRemaining)
+	{
+		$sRemaining = '';
+		if ( $dRemaining !== null && preg_match('/^\d{2,4}-\d{1,2}-\d{1,2}$/', $dRemaining) ) {
+			$dExpiration = new DateTime($dRemaining);
+			$dControl = new DateTime('');
+			$datectrl = $dControl->diff($dExpiration);
+			if ( $datectrl->format('%a') != 0 && $datectrl->format('%r%a') > -10000 ) {
+				$dDateNow = new DateTime(date('Y-m-d'));
+				$interval = $dDateNow->diff($dExpiration);
+				$sRemaining = $interval->format('%r%a');
+			}
+		}
+		return $sRemaining;
+	}
 
-    /**
-     * @inheritdoc
-     */
+	/**
+	 * @inheritdoc
+	 */
 
-    public function Get($sValue)
-    {
-        return Self::FormatRemainingDays($sValue);
-    }
+	public function Get($sValue)
+	{
+		return Self::FormatRemainingDays($sValue);
+	}
 
-    /**
-     * @inheritdoc
-     */
+	/**
+	 * @inheritdoc
+	 */
 
-    public function GetValueLabel($sValue, $oHostObject = null)
-    {
-        return Self::FormatRemainingDays($sValue);
-    }
+	public function GetValueLabel($sValue, $oHostObject = null)
+	{
+		return Self::FormatRemainingDays($sValue);
+	}
 
-    /**
-     * @inheritdoc
-     */
-    
-    public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
-    {
-        return Self::FormatRemainingDays($sValue);
-    }
+	/**
+	 * @inheritdoc
+	 */
+		
+	public function GetAsHTML($sValue, $oHostObject = null, $bLocalize = true)
+	{
+		return Self::FormatRemainingDays($sValue);
+	}
 
-    /**
-     * @inheritdoc
-     */
-    
-    public function GetAsXML($sAttCode, $bLocalize = true)
-    {
-        return Self::FormatRemainingDays($sValue);
-    }
+	/**
+	 * @inheritdoc
+	 */
+		
+	public function GetAsXML($sAttCode, $bLocalize = true)
+	{
+		return Self::FormatRemainingDays($sAttCode);
+	}
 }
